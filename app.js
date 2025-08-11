2c4fc3f675a4d422b3baaff597c976335f66fe19
Olimar Duarte <olimar.digital@gmail.com>
2025-08-11 17:00:50 -0400

    Implementar una función para agregar nombres de amigos válidos, añadirlos a un array y limpiar la caja de input

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+
+function agregarAmigo() {
+    let inputAmigo = document.getElementById('amigo').value;
+    console.log(inputAmigo);
+
+    if (inputAmigo == '') {
+        alert('Por favor, inserte un nombre.')
+    } else {
+        amigos.push(inputAmigo);
+        console.log(amigos);
+    }
+    limpiarCaja()
+}
+
+function limpiarCaja() {
+    document.getElementById('amigo').value = '';
+}
