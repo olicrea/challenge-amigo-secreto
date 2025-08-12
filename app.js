2b79022256eeeb5c34f1f1e63ac929dfbb201867
Olimar Duarte <olimar.digital@gmail.com>
2025-08-12 12:11:37 -0400

    Creada la función sortearAmigo()

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = []
-let ulListaAmigos = document.querySelector(".name-list");
+let ulListaAmigos = document.querySelector('.name-list');
+let ulListaResultado = document.querySelector('.result-list');
+
 
 function agregarAmigo() {
     let inputAmigo = document.getElementById('amigo').value;
@@ -26,11 +28,27 @@ function crearLista(){
     ulListaAmigos.innerHTML = '';
 
     for (let i = 0; i < amigos.length; i++) {
-        let liItemAmigo = document.createElement("li")
+        let liItemAmigo = document.createElement('li');
         liItemAmigo.innerHTML = amigos[i];
         console.log(liItemAmigo);
-        ulListaAmigos.appendChild(liItemAmigo)
+        ulListaAmigos.appendChild(liItemAmigo);
                 
+    }
+}
+
+function sortearAmigo() {
+    ulListaResultado.innerHTML = '';
+
+    console.log(amigos.length);
+    if (amigos.length === 0) {
+        alert('Por favor, inserta nombres para sortear.');
+    } else if (amigos.length < 2) {
+        alert('Por favor, inserta más de un nombre para sortear.');
+    } else {
+        let indiceRandomAmigo = Math.floor(Math.random() * amigos.length);
+        let amigoSorteado = amigos[indiceRandomAmigo];
+        console.log(amigoSorteado);
+        document.getElementById('resultado').innerHTML = amigoSorteado;
     }
 }
 
